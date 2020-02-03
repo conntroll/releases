c4a8c07ecd7aeea500be36c3fec4521eb4d5b586
navigaid <navigaid@gmail.com>
2020-02-04 03:44:00 +0800

    install.ps1: basic subst

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env pwsh
-# Copyright 2018 the Deno authors. All rights reserved. MIT license.
+# Copyright 2018 the K0s authors. All rights reserved. MIT license.
 # TODO(everyone): Keep this script simple and easily auditable.
 
 $ErrorActionPreference = 'Stop'
@@ -13,12 +13,12 @@ if ($PSVersionTable.PSEdition -ne 'Core') {
   $IsMacOS = $false
 }
 
-$DenoInstall = $env:DENO_INSTALL
-$BinDir = if ($DenoInstall) {
+$K0sInstall = $env:K0S_INSTALL
+$BinDir = if ($K0sInstall) {
   if ($IsWindows) {
-    "$DenoInstall\bin"
+    "$K0sInstall\bin"
   } else {
-    "$DenoInstall/bin"
+    "$K0sInstall/bin"
   }
 } elseif ($IsWindows) {
   "$Home\.deno\bin"
@@ -32,13 +32,13 @@ $Zip = if ($IsWindows) {
   'gz'
 }
 
-$DenoZip = if ($IsWindows) {
+$K0sZip = if ($IsWindows) {
   "$BinDir\deno.$Zip"
 } else {
   "$BinDir/deno.$Zip"
 }
 
-$DenoExe = if ($IsWindows) {
+$K0sExe = if ($IsWindows) {
   "$BinDir\deno.exe"
 } else {
   "$BinDir/deno"
@@ -57,7 +57,7 @@ $OS = if ($IsWindows) {
 # GitHub requires TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$DenoUri = if (!$Version) {
+$K0sUri = if (!$Version) {
   $Response = Invoke-WebRequest 'https://github.com/denoland/deno/releases' -UseBasicParsing
   if ($PSVersionTable.PSEdition -eq 'Core') {
     $Response.Links |
@@ -85,13 +85,13 @@ if (!(Test-Path $BinDir)) {
   New-Item $BinDir -ItemType Directory | Out-Null
 }
 
-Invoke-WebRequest $DenoUri -OutFile $DenoZip -UseBasicParsing
+Invoke-WebRequest $K0sUri -OutFile $K0sZip -UseBasicParsing
 
 if ($IsWindows) {
-  Expand-Archive $DenoZip -Destination $BinDir -Force
-  Remove-Item $DenoZip
+  Expand-Archive $K0sZip -Destination $BinDir -Force
+  Remove-Item $K0sZip
 } else {
-  gunzip -df $DenoZip
+  gunzip -df $K0sZip
 }
 
 if ($IsWindows) {
@@ -101,16 +101,16 @@ if ($IsWindows) {
     [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
     $Env:Path += ";$BinDir"
   }
-  Write-Output "Deno was installed successfully to $DenoExe"
+  Write-Output "K0s was installed successfully to $K0sExe"
   Write-Output "Run 'deno --help' to get started"
 } else {
   chmod +x "$BinDir/deno"
-  Write-Output "Deno was installed successfully to $DenoExe"
+  Write-Output "K0s was installed successfully to $K0sExe"
   if (Get-Command deno -ErrorAction SilentlyContinue) {
     Write-Output "Run 'deno --help' to get started"
   } else {
     Write-Output "Manually add the directory to your `$HOME/.bash_profile (or similar)"
     Write-Output "  export PATH=`"${BinDir}:`$PATH`""
-    Write-Output "Run '$DenoExe --help' to get started"
+    Write-Output "Run '$K0sExe --help' to get started"
   }
 }
